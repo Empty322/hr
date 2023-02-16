4e2852371d0b67ff5e5ade4f4414a1cf5160a5a2
Misha Bespalov <mishabespalov@yandex.ru>
2023-02-16 18:03:43 +0300

    added tests project

---
+++ hr.Tests/AutoMapperTests.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using hr.AutoMapper;
+
+namespace hr.Tests
+{
+	public class AutoMapperTests
+	{
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void AutomapperConfiguration_IsValid()
+		{
+			var config = new MapperConfiguration(cfg => {
+				cfg.AddProfile<AutoMapperProfile>();
+			});
+
+			config.AssertConfigurationIsValid();
+		}
+	}
+}
